a8146c247f259dfe39f1c74d73651372c0866e49
Rik Roots <rik.roots@gmail.com>
2014-08-03 20:43:15 +0100

    updating to version 3.1.6 - javascript linted and prettified

--- source/scrawlCore.js
+++ source/scrawlCore.js
@@ -25,7 +25,7 @@
 /**
 # scrawlCore
 
-## Version 3.1.5 - 30 July 2014
+## Version 3.1.6 - 3 August 2014
 
 Developed by Rik Roots - <rik.roots@gmail.com>, <rik@rikweb.org.uk>
 
@@ -106,7 +106,7 @@ Scrawl.js version number
 @default 3.1.4
 @final
 **/
-	my.version = '3.1.5';
+	my.version = '3.1.6';
 	/**
 Array of array object keys used to define the sections of the Scrawl library
 @property nameslist
