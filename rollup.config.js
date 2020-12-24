b4fd3a503f6dda5781474c3e3916aad32ca914a6
Rik Roots <rik.roots@gmail.com>
2020-12-24 11:49:24 +0000

    bump to v8.3.2

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ export default {
     format: 'es',
     plugins: [terser({
       mangle: {
-        reserved: ['Action', 'Anchor', 'Animation', 'Bezier', 'Block', 'Canvas', 'Cell', 'Color', 'Coordinate', 'Element', 'Emitter', 'Filter', 'FontAttributes', 'Force', 'Gradient', 'Grid', 'Group', 'ImageAsset', 'Line', 'Loom', 'Net', 'Oval', 'Palette', 'Particle', 'ParticleHistory', 'Pattern', 'Phrase', 'Picture', 'Polygon', 'Polyline', 'Quadratic', 'Quaternion', 'RadialGradient', 'Rectangle', 'RenderAnimation', 'Shape', 'Spiral', 'Spring', 'SpriteAsset', 'Stack', 'Star', 'State', 'Tetragon', 'Ticker', 'Tracer', 'Tween', 'UnstackedElement', 'Vector', 'VideoAsset', 'Wheel', 'World']
+        reserved: ['Action', 'Anchor', 'Animation', 'Bezier', 'Block', 'Canvas', 'Cell', 'Cog', 'Color', 'Coordinate', 'Element', 'Emitter', 'Filter', 'FontAttributes', 'Force', 'Gradient', 'Grid', 'Group', 'ImageAsset', 'Line', 'Loom', 'Net', 'Oval', 'Palette', 'Particle', 'ParticleHistory', 'Pattern', 'Phrase', 'Picture', 'Polygon', 'Polyline', 'Quadratic', 'Quaternion', 'RadialGradient', 'Rectangle', 'RenderAnimation', 'Shape', 'Spiral', 'Spring', 'SpriteAsset', 'Stack', 'Star', 'State', 'Tetragon', 'Ticker', 'Tracer', 'Tween', 'UnstackedElement', 'Vector', 'VideoAsset', 'Wheel', 'World']
       }
     })]
   }
