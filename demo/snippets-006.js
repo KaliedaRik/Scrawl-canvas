f75609d8c3d29c58fe095e0542d2188c927f4abd
Rik Roots <rik.roots@gmail.com>
2022-06-03 20:51:26 +0100

    fix documentation navigation

--- demo/snippets-006.js
+++ demo/snippets-006.js
@@ -2,13 +2,12 @@
 // Editable header element color font snippets
 //
 // Related files:
-// + [Editable header text colorizer and animation effect snippets](../snippets-006.html)
-// + [Animated highlight gradient text snippet](./animated-highlight-gradient-text-snippet.html)
-// + [Bubbles text snippet](./bubbles-text-snippet.html)
-// + [Risograph text gradient snippet](./risograph-text-gradient-snippet.html)
-// + [Swirling stripes text snippet](./swirling-stripes-text-snippet.html)
-// + [Worley text gradient snippet](./worley-text-gradient-snippet.html)
-// + [Text snippet helper](./text-snippet-helper.html)
+// + [Animated highlight gradient text snippet](./snippets/animated-highlight-gradient-text-snippet.html)
+// + [Bubbles text snippet](./snippets/bubbles-text-snippet.html)
+// + [Risograph text gradient snippet](./snippets/risograph-text-gradient-snippet.html)
+// + [Swirling stripes text snippet](./snippets/swirling-stripes-text-snippet.html)
+// + [Worley text gradient snippet](./snippets/worley-text-gradient-snippet.html)
+// + [Text snippet helper](./snippets/text-snippet-helper.html)
 //
 // [Run code](../../demo/snippets-006.html)
 
