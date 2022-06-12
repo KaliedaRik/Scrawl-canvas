ab5c05a9268c5bf2cc1dc2666ad0d058f903eafd
Rik Roots <rik.roots@gmail.com>
2022-06-12 19:05:45 +0100

    bump to v8.9.0; documentation

--- source/core/library.js
+++ source/core/library.js
@@ -9,7 +9,7 @@
 
 
 // Current version
-export const version = '8.8.2';
+export const version = '8.9.0';
 
 
 // Objects created using the __makeAnchor__ factory
--- source/scrawl.d.ts
+++ source/scrawl.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Scrawl-canvas 8.8.2
+// Type definitions for Scrawl-canvas 8.9.0
 
 
 
