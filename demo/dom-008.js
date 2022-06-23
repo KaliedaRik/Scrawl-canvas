23fd68b1852216e9dde671bd625c0c368104cabb
Rik Roots <rik.roots@gmail.com>
2022-06-23 19:36:43 +0100

    demo dom-008 update

--- demo/dom-008.js
+++ demo/dom-008.js
@@ -8,143 +8,259 @@ import { reportSpeed } from './utilities.js';
 
 
 // #### Scene setup
-let artefact = scrawl.library.artefact,
-    stack = artefact.mystack,
-    cube = artefact.cube;
+// Convenience labels - to save some typing
+const artefact = scrawl.library.artefact,
+  stack = artefact.mystack,
+  pin = artefact.pin,
+  front = artefact.frontface,
+  back = artefact.backface,
+  top = artefact.topface,
+  bottom = artefact.bottomface,
+  left = artefact.leftface,
+  right = artefact.rightface;
 
+const faces = scrawl.makeGroup({
+    name: "faces"
+}).addArtefacts(left, right, top, bottom, front, back);
 
-// The 'faces' Element artefacts are defined in the demo's `html` file (they are children of the Stack element). To make controlling them easier, we can create a Group object linked to the Stack and shift them into it.
-let faces = scrawl.makeGroup({
-    name: 'faces',
-    host: 'mystack',
-}).moveArtefactsIntoGroup('leftface', 'rightface', 'topface', 'bottomface', 'frontface', 'backface');
-
-
-// Give the Stack element some perspective depth
 stack.set({
-    perspectiveX: '50%',
-    perspectiveY: '50%',
+    perspectiveX: "50%",
+    perspectiveY: "50%",
     perspectiveZ: 1200
 });
 
-
-// The 'cube' Element artefact (also defined in html, but not shifted into our new group) will act as the reference artefact for our 'faces' Elements
-cube.set({
+pin.set({
     order: 1,
     width: 0,
     height: 0,
-    startX: 'center',
-    startY: 'center',
-    handleX: 'center',
-    handleY: 'center',
-    lockTo: 'start',
+    start: ["center", "center"],
+    lockTo: "start",
 
-    // Give the Element a border, so we can see it
-    css: {
-        border: '20px solid black',
-    },
-
-    // Delta animate the cube Element's rotation:
-    // + `pitch` represents rotation along the x-axis
-    // + `yaw` represents rotation along the y-axis
-    // + `roll` represents rotation along the z-axis
-    //
-    // Values are in degrees, not radians!
+    // To spin the cube we only need to rotate this element
+    // - canvases pivot to the cube element for position/rotation
     delta: {
-        pitch: 0.8,
-        yaw: 1.2,
-        roll: 0.4,
-    },
-});
-
-
-// Give our 'faces' Elements some common values, via their Group
-faces.setArtefacts({
-
-    // Make sure the Elements get processed _after_ the reference Element, so that they pick up changes to its positioning
-    order: 2,
-
-    width: 200,
-    height: 200,
-    startX: 'center',
-    startY: 'center',
-    handleX: 'center',
-    handleY: 'center',
-
-    // Offset each Element from the reference Element
-    offsetZ: 100,
-
-    // Set each Element to `pivot` to the reference Element
-    lockTo: 'pivot',
-    pivot: 'cube',
-
-    // Automatically add the reference Element's rotation values to each Element's initial rotation value (set below)
-    addPivotRotation: true,
-
-    // Styling common to all the 'faces' Elements
-    css: {
-        border: '1px solid blue',
-        textAlign: 'center',
-        backfaceVisibility: 'hidden',
+        pitch: 0.4,
+        yaw: 0.8,
+        roll: 0.2
     }
 });
 
+// Give our canvases some common values, via their Group
+faces.setArtefacts({
 
-// Give each 'faces' Element its own color and initial rotation
-artefact.frontface.set({
-    css: { backgroundColor: 'rgba(255, 0, 0, 0.4)' },
+    // Make sure the canvases get processed after the pin
+    // - so that they pick up changes to its positioning
+    order: 2,
+
+    trackHere: "local",
+
+    // Common dimensions; common start/handle values
+    width: 300,
+    height: 300,
+    baseMatchesCanvasDimensions: true,
+    start: ["center", "center"],
+    handle: ["center", "center"],
+
+    // Offset each canvas from the pin
+    offsetZ: 150,
+
+    // Set each canvas to pivot to the pin
+    lockTo: "pivot",
+    pivot: pin,
+
+    // Automatically add the pin's rotation values to each canvas's initial rotation value (set below)
+    addPivotRotation: true,
+
+    // Styling common to all the canvas elements
+    css: {
+        backfaceVisibility: "hidden"
+    }
 });
-artefact.rightface.set({
-    css: { backgroundColor: 'rgba(0, 0, 127, 0.4)' },
-    yaw: 90,
+
+// Give each canvas its initial rotation
+right.set({
+    yaw: 90
 });
-artefact.topface.set({
-    css: { backgroundColor: 'rgba(0, 255, 0, 0.4)' },
-    pitch: 90,
+top.set({
+    pitch: 90
 });
-artefact.backface.set({
-    css: { backgroundColor: 'rgba(127, 0, 0, 0.4)' },
-    pitch: 180,
+back.set({
+    pitch: 180
 });
-artefact.leftface.set({
-    css: { backgroundColor: 'rgba(0, 0, 255, 0.4)' },
-    yaw: 270,
+left.set({
+    yaw: 270
 });
-artefact.bottomface.set({
-    css: { backgroundColor: 'rgba(0, 127, 0, 0.4)' },
-    pitch: 270,
+bottom.set({
+    pitch: 270
 });
 
 
-// #### User interaction
-// For this demo we will suppress touchmove functionality over the canvas
-scrawl.addNativeListener(['touchmove'], (e) => {
+// Build some gradients to color the canvases
+scrawl.makeGradient({
 
-    e.preventDefault();
-    e.returnValue = false;
+    name: "red-gradient",
+    endX: "100%",
+    endY: "100%",
+    colors: [
+        [0, "#fff"],
+        [499, "#f00"],
+        [999, "#000"]
+    ]
 
-}, stack.domElement);
+}).clone({
 
-// Function to check whether mouse cursor is over the Stack element, and lock the cube Element accordingly
-let stackCheck = function () {
+    name: "green-gradient",
+    colors: [
+        [0, "#fff"],
+        [499, "#0f0"],
+        [999, "#000"]
+    ]
 
-    let active = false;
+}).clone({
 
-    return function () {
+    name: "blue-gradient",
+    colors: [
+        [0, "#fff"],
+        [499, "#00f"],
+        [999, "#000"]
+    ]
 
-        if (stack.here.active !== active) {
+}).clone({
 
-            active = stack.here.active;
+    name: "yellow-gradient",
+    colors: [
+        [0, "#fff"],
+        [499, "#ff0"],
+        [999, "#000"]
+    ]
 
-            cube.set({
-                lockTo: (active) ? 'mouse' : 'start'
-            });
-        }
-    };
-}();
+}).clone({
+
+    name: "magenta-gradient",
+    colors: [
+        [0, "#fff"],
+        [499, "#f0f"],
+        [999, "#000"]
+    ]
+
+}).clone({
+
+    name: "cyan-gradient",
+    colors: [
+        [0, "#fff"],
+        [499, "#0ff"],
+        [999, "#000"]
+    ]
+});
+
+// Display a gradient on each canvas using a Block entity
+scrawl.makeBlock({
+
+    name: "topface-block",
+    group: top.base.name,
+    dimensions: ["100%", "100%"],
+    start: ["center", "center"],
+    handle: ["center", "center"],
+    fillStyle: "red-gradient"
+
+}).clone({
+
+    name: "leftface-block",
+    group: left.base.name,
+    fillStyle: "green-gradient"
+
+}).clone({
+
+    name: "bottomface-block",
+    group: bottom.base.name,
+    fillStyle: "yellow-gradient",
+    roll: -90
+
+}).clone({
+
+    name: "rightface-block",
+    group: right.base.name,
+    fillStyle: "magenta-gradient",
+    roll: 90
+
+}).clone({
+
+    name: "frontface-block",
+    group: front.base.name,
+    fillStyle: "cyan-gradient",
+    roll: 0
+
+}).clone({
+
+    name: "backface-block",
+    group: back.base.name,
+    fillStyle: "blue-gradient",
+    roll: -90
+});
+
+// Add a label to each canvas using a Phrase entity
+scrawl
+  .makePhrase({
+    name: "topface-label",
+    group: top.base.name,
+    start: ["center", "center"],
+    handle: ["center", "center"],
+    text: "TOP",
+    family: "sans-serif",
+    weight: "bold",
+    size: "3em",
+    lineHeight: 0,
+    fillStyle: "yellow",
+    lineWidth: 2,
+    method: "fillThenDraw"
+  })
+  .clone({
+    name: "bottomface-label",
+    group: bottom.base.name,
+    text: "BOTTOM"
+  })
+  .clone({
+    name: "leftface-label",
+    group: left.base.name,
+    text: "LEFT"
+  })
+  .clone({
+    name: "rightface-label",
+    group: right.base.name,
+    text: "RIGHT"
+  })
+  .clone({
+    name: "frontface-label",
+    group: front.base.name,
+    text: "FRONT"
+  })
+  .clone({
+    name: "backface-label",
+    group: back.base.name,
+    text: "BACK"
+  });
 
 
 // #### Scene animation
+const stackCheck = (function () {
+
+    let stackActive = false,
+        here = stack.here;
+
+    return function () {
+
+        if (here.active != stackActive) {
+
+            stackActive = here.active;
+
+            pin.set({
+                lockTo: (stackActive) ? 'mouse' : 'start',
+            });
+        }
+    }
+})();
+
+
 // Function to display frames-per-second data, and other information relevant to the demo
 const report = reportSpeed('#reportmessage');
 
@@ -152,10 +268,16 @@ const report = reportSpeed('#reportmessage');
 // Create the Display cycle animation
 scrawl.makeRender({
 
-    name: 'demo-animation',
+    name: 'demo-animation-stack',
     target: stack,
     commence: stackCheck,
     afterShow: report,
+});
+
+scrawl.makeRender({
+
+    name: 'demo-animation-canvases',
+    target: [top, bottom, left, right, front, back],
 });
 
 
